0829fdbc2a313ce808ea02760a1cb4f98a9a3bc8
AnitaLviv <annet_v@ukr.net>
2016-12-07 20:37:09 +0200

    exam

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+
+var gulp = require('gulp');
+var cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+
+
+gulp.task('default', function() {
+   gulp.src('stylesheets/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('app'));
+});
+
+gulp.task('default', function() {
+  gulp.src('js/**/*.js')
+    .pipe(concat('all.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('app/'))
+});
+
+
+gulp.task('default', function(){
+gulp.src('src/img/*')
+     .pipe(imagemin({progressive: true, optimizationLevel: 7}))
+     .pipe(gulp.dest('app/img'));
+});
+
+gulp.task('watch', function(){
+gulp.watch('src/**/*.js',['default']);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
